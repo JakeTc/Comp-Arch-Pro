54671da57b09431ba8b516730fda2526fbf1c987
JakeTc <tcjake863@gmail.com>
2019-10-01 04:16:09 -0400

    mallocing the array not working

--- pa1/fourth/fourth.c
+++ pa1/fourth/fourth.c
@@ -23,13 +23,109 @@ int badMultiply(int m1r, int m1c, int m2r, int m2c) {
 	return 0;
 }
 
+//for debugging
+void printArr(int *arr, int length) {
+	int i = 0;
+
+	for(i = 0; i < length; i++) {
+		printf("%d\t", arr[i]);
+	}
+	printf("\n");
+}
+
+//for MORE debugging
+void print2DArr(int **arr, int width, int length) {
+	int i = 0;
+	for(i = 0; i < width; i++) {
+		printArr(arr[i], length);
+	}
+	printf("\n");
+}
 
 int main(int argc, char* argv[]) {
+	//testing!
 	int arr1[] = {1, 2, 3};
 	int arr2[] = {7, 9, 11};
 	int ans = dotProduct(arr1, arr2, 3);
 	
 	printf("The ans is: %d\n", ans);
 	
+	//Make the file
+	if(argc == 1) {
+		printf("Error: no arguments given\n");
+		return 0;
+	}
+	
+	FILE *fp = fopen(argv[1], "r");
+	
+	if(fp == NULL) {
+		printf("Error: File could not be opened\n");
+		return 0;
+	}
+	
+	//reads the rows and columns of the array
+	int m1r;
+	int m1c;
+	fscanf(fp, "%d\t%d", &m1r, &m1c);
+	
+	printf("this gai %d\n", m1r);
+	int **matrix1 = (int**)malloc(m1r * sizeof(int*));
+	int i = 0;
+	for(i = 0; i < m1c; i++) {
+		matrix1[i] = malloc(m1c * sizeof(int));
+	}
+//ERROR LIES BEFORE THIS!	
+	
+	printf("this works\n");
+	
+	int j = 0;
+	for(i = 0; i < m1r; i++) {
+		for(j = 0; j < m1c; j++) {
+			//do not need to check for edge cases
+			fscanf(fp, "%d", &matrix1[i][j]);
+		}
+		
+		
+		
+	}
+	print2DArr(matrix1, m1r, m1c);
+	
+	
+	//free matrix1
+	for(i = 0; i < m1r; i++) {
+		free(matrix1[i]);
+	}
+	free(matrix1);
+	
+	//free matrix2
+	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
