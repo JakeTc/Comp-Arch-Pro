81f4698d3cc2795735b4233414a728ecb3e1998f
JakeTc <tcjake863@gmail.com>
2019-10-02 01:18:53 -0400

    added foundation to third.c

---
+++ autograder/pa1/sixth/sixth.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _Leaf {
+	struct _Leaf *left;
+	struct _Leaf *right;
+	int key;
+	
+} Leaf;
+
+//makes a new Leaf and returns a pointer to it
+Leaf *mkLeaf(int nwkey) {
+	Leaf *nwLeaf = malloc(sizeof(Leaf));
+	nwLeaf->key = nwkey;
+	nwLeaf->left = NULL;
+	nwLeaf->right = NULL;
+	
+	return nwLeaf;
+}
+
+//recursively look for a space to put the leaf
+Leaf *addLeaf(Leaf *stem, int nwkey) {
+	if(stem == NULL) {
+		Leaf *nwLeaf = mkLeaf(nwkey);
+		return nwLeaf;
+	} else {
+		
+		//if current key is more than nwkey, stores it to the left
+		//if current key is less than nwkey, stores it to the right
+		//else, does nothing (if equal)
+		if(stem->key > nwkey) {
+			stem->left = addLeaf(stem->left, nwkey);
+		} else if(stem->key < nwkey) {
+			stem->right = addLeaf(stem->right, nwkey);
+		}
+	}
+	
+	return stem;
+}
+
+//recursively print tree
+void printTree(Leaf *stem) {
+	if(stem == NULL) {
+		return;
+	}
+	
+	printTree(stem->left);
+	printf("%d\t", stem->key);
+	printTree(stem->right);
+}
+
+int main(int argc, char *argv[]) {
+	Leaf *head = NULL;
+	/*
+	//testing!
+	head = addLeaf(head, 5);
+	head = addLeaf(head, 4);
+	head = addLeaf(head, 7);
+	head = addLeaf(head, 6);
+	head = addLeaf(head, 10);
+	head = addLeaf(head, 2);
+	head = addLeaf(head, 3);
+	*/
+	
+	if(argc == 1) {
+		printf("Insufficient amount of arguments!\n");
+		return 0;
+	}
+	
+	FILE *fp = fopen(argv[1], "r");
+
+	if(fp == NULL) {
+		printf("File could not be read!\n");
+		return 0;
+	}
+	
+	char temp[100];
+	int data;
+	
+	while(fscanf(fp, "%s\t%d", temp, &data) != EOF) {
+		head = addLeaf(head, data);
+	}
+	
+	//prints out tree
+	printTree(head);
+	printf("\n");
+	//if file doesnt exist, just print error
+	
+	return 0;
+}
--- pa1/third/third.c
+++ pa1/third/third.c
@@ -1,9 +1,117 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct _Node {
-	void *data;
+	int data;
 	struct _Node *next;
 } Node;
+
+//makes a node and returns a pointer to it
+Node *mkNode(int data) {
+	Node *nwNode = malloc(sizeof(Node));
+	nwNode->data = data;
+	nwNode->next = NULL;
+
+	return nwNode;
+}
+
+//deletes the entirety of a linked list!
+void deleteLL(Node *head) {
+	Node *current = head;
+	Node *next;
+	
+	while(current != NULL) {
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
+//coutns the amount of nodes that are in the Linked List
+void countLL(Node *head) {
+	Node *ptr = head;
+	int counter = 0;
+	while(ptr != NULL) {
+		counter++;
+		ptr = ptr->next;
+	}
+	
+	printf("%d\n", counter);
+}
+
+//for testing
+void pLL(Node *head) {
+	Node *ptr = head;
+	
+	while(ptr != NULL) {
+		printf("%d\t", ptr->data);
+		ptr = ptr->next;
+	}
+	printf("\n");
+	
+}
+
+int getKey(int data) {
+	int key = data % 10000;
+	
+	if(key < 0) {
+		key = key + 10000;
+	}
+	
+	return key;
+}
+
+//for inserting into the hastable
+int insertHash(Node *hashtable[], int data) {
+	
+	//gets the key and sees if the key is in the array
+	int key = getKey(data);
+	if(hashtable[key] == NULL) {
+		hashtable[key] = mkNode(key);
+		
+	} else {
+		
+	}
+	
+	
+	
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	Node **hashtable = calloc(10000, sizeof(Node*));
+	int i = 0;
+	
+	
+	//free used elements of hashtable
+	
+	//free the hashtable
+	free(hashtable);
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
